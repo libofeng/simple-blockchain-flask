02a119e882d6837e39ab355a906430545d645fe7
Bofeng Li <bofeng.li@outlook.com>
2018-04-22 21:13:55 +0800

    Add new transaction.

--- Blockchain.py
+++ Blockchain.py
@@ -7,9 +7,22 @@ class Blockchain(object):
         # Creates a new Block and adds it to the chain pass
         pass
 
-    def new_transaction(self):
-        # Adds a new transaction to the list of  transactions
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+        生成新交易信息，信息将加入到下一个待挖的区块中
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hod this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
